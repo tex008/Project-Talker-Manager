aeaf1e72cdc57a7ccb4edc79e1c21d8112e86bd1
Vinicius Teixeira Freitas <texera009@gmail.com>
2022-08-03 18:04:32 -0300

    feat: adiciona requisito 5 ao projeto. Constroi middlewares de validação do novo palestrante, e adiciona novo palestrante ao db

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const fs = require('fs').promises;
-const crypto = require('crypto');
+const fs = require('fs');
 const bodyParser = require('body-parser');
+
+const loginValidations = require('./login.js');
+const talkerValidations = require('./talker.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,64 +13,16 @@ const PORT = '3000';
 const { log } = console;
 const talkersData = 'talker.json';
 
-function generateRandomToken() {
-  return crypto.randomBytes(8).toString('hex');
-}
-
-function validateEmail(req, res, next) {
-  const { email } = req.body;
-  if (!email || email === '') { 
-    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-  }
-  next();
-}
-
-function checkValidEmail(req, res, next) {
-  const { email } = req.body;
-  const pattern = /\S+@\S+.com/;
-  const isEmailValid = pattern.test(email);
-  if (!isEmailValid) { 
-    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-}
-  next();
-}
-
-function validatePassword(req, res, next) {
-  const { password } = req.body;
-  if (!password || password === '') { 
-    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  }
-  next();
-}
-
-function checkValidPassword(req, res, next) {
-  const { password } = req.body;
-  log(password.length);
-  const PASSWORD_MIN_LENGTH = 6;
-  if (password.length < PASSWORD_MIN_LENGTH) {
-    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  }
-  next();
-}
-
-app.get('/talker', async (req, res) => {
-  try {
-    const talkers = await fs.readFile(talkersData, 'utf-8');
-    return res.status(HTTP_OK_STATUS).json(JSON.parse(talkers));
-  } catch (error) {
-    log(error);
-  }
+app.get('/talker', (req, res) => {
+  const talkers = JSON.parse(fs.readFileSync(talkersData, 'utf-8'));
+  return res.status(HTTP_OK_STATUS).json(talkers);
 });
 
-app.get('/talker/:id', async (req, res) => {
-  try {
-    const talkers = await fs.readFile(talkersData, 'utf-8');
-    log(talkers);
+app.get('/talker/:id', (req, res) => {
+    const talkers = fs.readFileSync(talkersData, 'utf-8');
     const { id } = req.params;
-    log(id);
 
     const parsedTalkers = JSON.parse(talkers);
-
     const talkerData = parsedTalkers.find((talker) => talker.id === Number(id));
 
     if (!talkerData) {
@@ -76,19 +30,37 @@ app.get('/talker/:id', async (req, res) => {
     }
 
     return res.status(HTTP_OK_STATUS).json(talkerData);
-  } catch (error) {
-    log(error);
-  }
 });
 
 app.post('/login',
-  validateEmail,
-  checkValidEmail,
-  validatePassword,
-  checkValidPassword,
+  loginValidations.validateEmail,
+  loginValidations.checkValidEmail,
+  loginValidations.validatePassword,
+  loginValidations.checkValidPassword,
   (req, res) => {
-  const newToken = generateRandomToken();
+  const newToken = loginValidations.generateRandomToken();
   res.status(HTTP_OK_STATUS).json({ token: `${newToken}` });
+});
+
+app.post('/talker',
+  talkerValidations.validateToken,
+  talkerValidations.validateTalkerName,
+  talkerValidations.validateTalkerAge,
+  talkerValidations.validateTalkerTalk,
+  talkerValidations.validateTalkRate,
+  talkerValidations.validateTalkWatchedAt,
+  (req, res) => {
+  const { body } = req;
+  const talkers = JSON.parse(fs.readFileSync(talkersData, 'utf-8'));
+  const id = talkers.length + 1;
+  const newTalker = {
+    id,
+    ...body,
+  };
+  log(talkers, newTalker);
+  talkers.push(newTalker);
+  fs.writeFileSync('talker.json', JSON.stringify(talkers));
+  res.status(201).json(newTalker);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
