ca9fe42c6d326bdb99c5c04d89dc69567b95606b
Vinicius Teixeira Freitas <texera009@gmail.com>
2022-08-03 14:42:35 -0300

    feat: adiciona requsiito 4 ao projeto. Cria middlewares de verificação de email e password

--- index.js
+++ index.js
@@ -15,6 +15,42 @@ function generateRandomToken() {
   return crypto.randomBytes(8).toString('hex');
 }
 
+function validateEmail(req, res, next) {
+  const { email } = req.body;
+  if (!email || email === '') { 
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  next();
+}
+
+function checkValidEmail(req, res, next) {
+  const { email } = req.body;
+  const pattern = /\S+@\S+.com/;
+  const isEmailValid = pattern.test(email);
+  if (!isEmailValid) { 
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+}
+  next();
+}
+
+function validatePassword(req, res, next) {
+  const { password } = req.body;
+  if (!password || password === '') { 
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  next();
+}
+
+function checkValidPassword(req, res, next) {
+  const { password } = req.body;
+  log(password.length);
+  const PASSWORD_MIN_LENGTH = 6;
+  if (password.length < PASSWORD_MIN_LENGTH) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+}
+
 app.get('/talker', async (req, res) => {
   try {
     const talkers = await fs.readFile(talkersData, 'utf-8');
@@ -45,7 +81,12 @@ app.get('/talker/:id', async (req, res) => {
   }
 });
 
-app.post('/login', (req, res) => {
+app.post('/login',
+  validateEmail,
+  checkValidEmail,
+  validatePassword,
+  checkValidPassword,
+  (req, res) => {
   const newToken = generateRandomToken();
   res.status(HTTP_OK_STATUS).json({ token: `${newToken}` });
 });
